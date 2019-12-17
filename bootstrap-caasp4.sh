753174d6120275622952267e0eaaef4a45e36de9
jmlambert78 <jmlambert78@gmail.com>
2019-12-17 11:18:42 +0100

    add crlf after kube entry in certSAN

--- bootstrap-caasp4.sh
+++ bootstrap-caasp4.sh
@@ -16,7 +16,7 @@ ssh-keyscan -t rsa $NODE2  >> ~/.ssh/known_hosts
 skuba  cluster init --control-plane $MASTER1  caaspV4
 cd caaspV4
 # add a SAN route in addition 
-sed -i '/certSANs/a\ \ - kube.cf1.jmllabsuse.com' kubeadm-init.conf
+sed -i '/certSANs/a\ \ - kube.cf1.jmllabsuse.com\n' kubeadm-init.conf
 # Edit the kubeadm-init.conf to add any SAN entry for certificates generation during the bootstrap.
 skuba node bootstrap --sudo --user jmlambert  --target $MASTER1 master1 -v10
 skuba node join --role worker --sudo --user jmlambert  --target $NODE1 node1 -v10
